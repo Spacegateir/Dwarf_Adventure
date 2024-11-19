7ee1cbf9080cbb8162aaacee9f12dbf3ff6f692b
spacegateir <1Spacegateir@gmail.com>
2024-11-19 19:16:53 +1030

    .

--- src/main/java/net/spacegateir/dwarfadventuremod/block/traps/StoneTrapBlock.java
+++ src/main/java/net/spacegateir/dwarfadventuremod/block/traps/StoneTrapBlock.java
@@ -6,12 +6,20 @@ import net.minecraft.entity.Entity;
 import net.minecraft.entity.effect.StatusEffectInstance;
 import net.minecraft.entity.effect.StatusEffects;
 import net.minecraft.server.world.ServerWorld;
+import net.minecraft.sound.SoundCategory;
+import net.minecraft.sound.SoundEvents;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.random.Random;
 import net.minecraft.world.World;
 import net.spacegateir.dwarfadventuremod.block.ModBlocks;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class StoneTrapBlock extends Block {
+    // Map to track the last time a sound was played for each block position
+    private static final Map<BlockPos, Long> soundCooldowns = new HashMap<>();
+
     public StoneTrapBlock(Settings settings) {
         super(settings);
     }
@@ -29,10 +37,28 @@ public class StoneTrapBlock extends Block {
                 world.scheduleBlockTick(pos, ModBlocks.STONE_TRAP_BLOCK_SPIKES, 20);
 
             } else if (state.isOf(ModBlocks.STONE_TRAP_BLOCK_SPIKES)) {
-                // Apply Slowness effect to the entity
+                // Apply status effects to the entity
                 if (entity instanceof net.minecraft.entity.LivingEntity livingEntity) {
                     livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.SLOWNESS, 100, 0));
                     livingEntity.addStatusEffect(new StatusEffectInstance(StatusEffects.INSTANT_DAMAGE, 5, 0));
+
+                    // Check if enough time has passed since the last sound was played
+                    long currentTime = world.getTime();
+                    Long lastSoundTime = soundCooldowns.getOrDefault(pos, 0L);
+
+                    if (currentTime - lastSoundTime >= 20) { // 20 ticks = 1 second
+                        // Play sound effect
+                        world.playSound(
+                                null,                          // Player responsible for the sound; null means no player
+                                pos,                           // Position of the sound
+                                SoundEvents.ENTITY_GENERIC_HURT, // The sound effect to play
+                                SoundCategory.BLOCKS,          // Category of the sound
+                                5.0F,                          // Volume
+                                1.0F                           // Pitch
+                        );
+                        // Update the last sound time
+                        soundCooldowns.put(pos, currentTime);
+                    }
                 }
             }
         }
